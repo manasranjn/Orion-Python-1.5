dd22c64b3d57b4c5f827e244507cc640da487d3f
Manas Ranjan Bera <manasbera484@gmail.com>
2026-04-17 13:33:32 +0530

    data types

--- Readme.md
+++ Readme.md
@@ -1,1 +1,55 @@
-# Programming language
+# What is Programming ?
+
+## Complier
+## Interpreter
+
+# Types of Programming Languages
+1. Low Level Language- Assembly Language, Machine Code
+2. High Level Language- 
+
+# What is Python ?
+Guido van Rossum in 1991
+
+## Features of Python-
+
+## What is Syntax?
+
+Print()
+
+# Comments
+1. Single Line Comment- #
+2. Multi Line Comment- """  """/'''  '''
+
+# Variable
+
+first_name
+
+# Data Type-
+## Number Types-
+1. int - 
+2. float - 
+3. complex - 
+
+## Sequence Types-
+1. String - s = "hello"
+2. List - l1 = [1,2,3,4,5]
+3. Tupple - t1 = (1,2,3,4,5,6)
+
+## Mapping Types-
+1. Dictionary - 
+student = {
+	'name': "Smith",
+	'age': 20,
+	'marks': [50,60,70]
+}
+
+## Boolean Type-
+1. Boolean - True/False
+
+## Set Type-
+1. Set - s1 = {1,1,1,2,2,3,3,4,4,5,5,6,78}
+
+## None Type-
+x = None
+
+type()
---
+++ day2.py
@@ -0,0 +1,69 @@
+#   ("This is my first Python code!")
+
+'''
+This is 
+multi-line comment
+in Python
+'''
+
+"""
+
+"""
+
+
+# Varibales and Data Types
+## Number Types-
+# 1. int
+a = 10
+# print(type(a))
+
+# 2. float
+f = 3.14
+# print(type(f))
+
+# 3. complex
+c = 1 + 2j
+# print(type(c))
+
+# Boolean Type-
+b1 = True
+b2 = False
+# print(type(b1))
+# print(type(b2))
+
+## Sequence Types-
+# 1. string
+s = "Hello World"
+# print(type(s))
+
+# 2. list
+l1 = [1, 2, 3, 4, 5]
+# print(type(l1))
+# print(l1)
+# print(l1[0])
+
+# 3. tuple
+t1 = (1, 2, 3, 4, 5)
+# print(type(t1))
+# print(t1)
+# print(t1[0])
+
+## Mapping Types-
+# 1. Dictionary -
+user = {
+    "name": "John Doe",
+    "age": 30,
+    "email": "yVb8D@example.com"
+}
+# print(type(user))
+# print(user)
+# print(user["name"])
+
+## Set Type-
+x = {1,1,1,2,3,3,4,4,5,5,6}
+# print(type(x))
+# print(x)
+
+## None Type-
+x = None
+print(type(x))
